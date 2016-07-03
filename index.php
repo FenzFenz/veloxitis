d9c4cebb75dc606ded8337e8444fe56363fc3d4a
link0087 <fabio@PCRIDDU-ASUS>
2016-07-03 16:05:27 +0200

    spazi

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -32,12 +31,6 @@
     <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container-fluid">
         <div class="navbar-header">
-<!--           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> -->
-<!--             <span class="sr-only">Toggle navigation</span> -->
-<!--             <span class="icon-bar"></span> -->
-<!--             <span class="icon-bar"></span> -->
-<!--             <span class="icon-bar"></span> -->
-<!--           </button> -->
           <a class="navbar-brand" href="#">Veloxitis</a>
         </div>
       </div>
